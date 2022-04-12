0eb746dfee6958813294d879e0b3948233cf008e
HavenchykA <elena.govenchik@gmail.com>
2022-04-12 20:59:56 +0300

    Initial commit

---
+++ src/TestTest.java
@@ -0,0 +1,28 @@
+
+public class TestTest {
+   public static void main(String[]args) {
+
+
+       double a = 17;
+       int b = 5;
+
+       double v = 5.68;
+
+       double result= a % 2;
+       System.out.println(result);
+     int q = 3;
+     int w = 2;
+     int e = 1;
+     String r = "GO!!!";
+     System.out.println(q);
+     System.out.println(w);
+     System.out.println(e);
+     System.out.println(r);
+     System.out.println("_");
+
+
+
+   }
+
+
+}
