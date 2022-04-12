a03f9c003dd1e4afc627e84a9eb0515b93f3c58e
HavenchykA <elena.govenchik@gmail.com>
2022-04-12 20:59:56 +0300

    Initial commit

---
+++ src/TestTest.java
@@ -0,0 +1,21 @@
+
+public class TestTest {
+   public static void main(String[]args) {
+
+
+       int a = 17;
+       int b = 5;
+
+
+     System.out.println(a+b);
+     System.out.println(a/b);
+     System.out.println(a*b);
+     System.out.println(a-b);
+
+
+
+
+   }
+
+
+}
